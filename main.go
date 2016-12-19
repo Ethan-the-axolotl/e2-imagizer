a49ee65847bf5b4759b54fbc2a5a96b724267bcb
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2016-12-19 01:23:42 -0800

    Wrote all the codez ( but probably with bugs )

---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"image"
+	"image/draw"
+	_ "image/jpeg"
+	_ "image/png"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/nfnt/resize"
+)
+
+func main() {
+	var host string
+	var port string
+	flag.StringVar(&host, "host", "localhost", "host to listen on for the webserver")
+	flag.StringVar(&port, "port", "8000", "port to listen on for the webserver")
+	flag.Parse()
+	http.HandleFunc("/", serve)
+	bind := fmt.Sprintf("%s:%s", host, port)
+	fmt.Printf("listening on %s...\n", bind)
+	err := http.ListenAndServe(bind, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func serve(response http.ResponseWriter, request *http.Request) {
+	target := request.URL.Query().Get("q")
+	u, err := url.Parse(target) // Parse the raw URL value we were given into somthing we can work with
+	if err != nil {
+		http.Error(response, err.Error(), 400)
+	}
+
+	if !u.IsAbs() { // Is our URL absolute or not?
+		u.Scheme = "http"
+	} else { // If our URL is absolute, make sure the protocol is http(s)
+		if u.Scheme != "http" || u.Scheme != "https" {
+			u.Scheme = "http"
+		}
+	}
+	targetFormatted := u.String() // Turn our type URL back into a nice easy string, and store it in a variable
+	fetchedResponse, err := http.Get(targetFormatted)
+	fetchedBody, err := ioutil.ReadAll(fetchedResponse.Body) // Read the response into another variable
+	detectedContentType := http.DetectContentType(fetchedBody)
+	parsedContentType, err := parseContentType(detectedContentType)
+	if err != nil {
+		http.Error(response, err.Error(), 500)
+	}
+	if parsedContentType.Type == "image" {
+		if parsedContentType.Subtype == "jpeg" || parsedContentType.Subtype == "png" {
+			serialized, err := serializeImage(strings.NewReader(string(fetchedBody)))
+			if err != nil {
+				http.Error(response, err.Error(), 500)
+			}
+			fmt.Fprint(response, serialized)
+		}
+	}
+}
+
+func serializeImage(reader io.Reader) (string, error) {
+	var output string
+
+	img, _, err := image.Decode(reader)
+	if err != nil {
+		return "", err
+	}
+	m := resize.Thumbnail(512, 512, img, resize.Lanczos3)
+	rect := m.Bounds()
+	rgba := image.NewRGBA(rect)
+	draw.Draw(rgba, rect, m, rect.Min, draw.Src)
+	output += strconv.Itoa(rect.Max.X) + " " + strconv.Itoa(rect.Max.Y) + "|"
+	for e := 0; e <= rect.Max.Y-1; e++ {
+		for i := 0; i <= rect.Max.X-1; i++ {
+			offset := rgba.PixOffset(i, e)
+			values := [3]int{int(rgba.Pix[offset]), int(rgba.Pix[offset+1]), int(rgba.Pix[offset+2])}
+			output += strconv.Itoa(values[0]) + "," + strconv.Itoa(values[1]) + "," + strconv.Itoa(values[2]) + " "
+		}
+	}
+	return output, nil
+}
+
+type ContentType struct { // The ContentType type holds easily usable information that is normally held as a string for indentifying MIME type and character encoding along with other information
+	Type       string            // The first part of the MIME type (eg. "text")
+	Subtype    string            // The second part of the MIME type (eg. "html")
+	Parameters map[string]string // Any extra information (eg. "charset=utf8") represeted as a map
+}
+
+func parseContentType(rawcontype string) (*ContentType, error) { // Parse a MIME string into a ContentType struct
+	rawcontype = strings.ToLower(rawcontype)
+	var contentType ContentType
+	contentType.Parameters = make(map[string]string)
+	contype := strings.Split(rawcontype, " ")
+	contype[0] = strings.Replace(contype[0], ";", "", -1)
+	mimetype := strings.Split(contype[0], "/")
+	if len(mimetype) <= 1 {
+
+		return new(ContentType), errors.New("contype: malformed content-type MIME type provided")
+	}
+	if len(contype) > 1 {
+		params := strings.Split(contype[1], ";")
+		for it := range params {
+			splitparams := strings.Split(params[it], "=")
+			contentType.Parameters[splitparams[0]] = splitparams[1]
+		}
+	}
+	contentType.Type = mimetype[0]
+	contentType.Subtype = mimetype[1]
+	return &contentType, nil
+}
